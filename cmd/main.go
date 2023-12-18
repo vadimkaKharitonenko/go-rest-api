7e230069dc9864ed27df6556c20ea6a3d9dab22b
Vadim Kharitonenko <vadimka.kharitonenko@mail.ru>
2023-12-18 13:07:52 +0300

    - repository, service, handler

--- cmd/main.go
+++ cmd/main.go
@@ -5,10 +5,15 @@ import (
 
 	todo "github.com/vadimkaKharitonenko/go-rest-api"
 	"github.com/vadimkaKharitonenko/go-rest-api/pkg/handler"
+	"github.com/vadimkaKharitonenko/go-rest-api/pkg/repository"
+	"github.com/vadimkaKharitonenko/go-rest-api/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error accured while running http server: %s", err.Error())

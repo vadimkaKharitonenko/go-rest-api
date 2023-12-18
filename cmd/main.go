bb93b72ae0556e3f6aa06a49a7b28789e1148537
Vadim Kharitonenko <vadimka.kharitonenko@mail.ru>
2023-12-18 14:37:13 +0300

    - migrations, configs

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	todo "github.com/vadimkaKharitonenko/go-rest-api"
 	"github.com/vadimkaKharitonenko/go-rest-api/pkg/handler"
 	"github.com/vadimkaKharitonenko/go-rest-api/pkg/repository"
@@ -10,12 +11,22 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing config: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error accured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
